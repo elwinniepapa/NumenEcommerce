2463c056bc9c1185c4a6372876cc054aea72a2a3
Facundo Cepeda <facundogacepeda@gmail.com>
2023-03-19 14:29:27 -0300

    Actualizado Firebase Link

--- src/assets/firebase.js
+++ src/assets/firebase.js
@@ -3,13 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, addDoc, collection, getDocs, getDoc, doc, updateDoc, deleteDoc } from "firebase/firestore"
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-    apiKey: "AIzaSyB7VvRzJypreMiyW_Dct1Iqzkz2tRkOif0",
-    authDomain: "numenecommerce-a72e2.firebaseapp.com",
-    projectId: "numenecommerce-a72e2",
-    storageBucket: "numenecommerce-a72e2.appspot.com",
-    messagingSenderId: "831805861306",
-    appId: "1:831805861306:web:e68e28bf0181a5639e8183"
+    apiKey: "AIzaSyDDhEpeJ-AelAHBf0RmtEFiD51FfLH4y-k",
+    authDomain: "ecommerce-52c12.firebaseapp.com",
+    projectId: "ecommerce-52c12",
+    storageBucket: "ecommerce-52c12.appspot.com",
+    messagingSenderId: "70673323625",
+    appId: "1:70673323625:web:5b6f387eda995a3b1df4f8"
 };
 
 // Initialize Firebase
@@ -24,7 +25,7 @@ export const addBDD = async () => { //async le decis que es consulta asincronica
     const promise = await fetch("./json/products.json") //await que la linea de la derecha es asincronica    
     const products = await promise.json() //es asincronica, no se cuanto va a llevar realizarla
     products.forEach(async (prod) => {
-        await addDoc(collection(db, 'productos'), {            
+        await addDoc(collection(db, 'productos'), {
             idCategoria: prod.idCategoria,
             nombre: prod.nombre,
             marca: prod.marca,
@@ -43,7 +44,7 @@ export const addBDD = async () => { //async le decis que es consulta asincronica
 
 export const getProducts = async () => {
     const items = await getDocs(collection(db, 'productos'))
-    const products = items.docs.map( prod => {
+    const products = items.docs.map(prod => {
         return { ...prod.data(), id: prod.id } //me permite transformar el objeto en array, con el operador pre hago una copia
     })
     return products
